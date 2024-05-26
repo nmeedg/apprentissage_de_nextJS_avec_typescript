650b7a89606a607e420c93b7229c985a7f2d193c
Ngoufack Edgard <ngoufackedgard1@gmail.com>
2024-05-26 11:51:50 +0100

    page 404 en nextjs

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div>
+        <h1>Page not found</h1>
+        <h2>404 | The server can not find this page </h2>
+    </div>
+  )
+}
--- src/app/products/[productId]/reviews/[reviewsId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewsId]/page.tsx
@@ -1,7 +1,16 @@
-export default function ReviewsDetails({params}:{params: {reviewsId:string,productId:string}}) {
-    return (
-        <h1>
-            Reviews {params.reviewsId} about product {params.productId}
-        </h1>
-    )
+import { notFound } from "next/navigation";
+
+export default function ReviewsDetails({
+  params,
+}: {
+  params: { reviewsId: string; productId: string };
+}) {
+    if (parseInt(params.reviewsId) > 100) {
+        notFound()
+    }
+  return (
+    <h1>
+      Reviews {params.reviewsId} about product {params.productId}
+    </h1>
+  );
 }
