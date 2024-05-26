d52b8ca4b966517554a4311644b9ed73c892b4ea
Ngoufack Edgard <ngoufackedgard1@gmail.com>
2024-05-26 21:30:04 +0100

    loading file

---
+++ src/app/blog/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <div className="flex justify-center w-full h-full mt-10">
+        <div className=" bg-blue-600 text-white p-5 h-[100px] max-w-min rounded-full animate-spin">
+      Loading
+    </div>
+    </div>
+  );
+}
